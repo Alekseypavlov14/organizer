f9344e175c5b5d4fa40f90d6065da5fe1018688d
Alekseypavlov14 <aleshapavlov9@gmail.com>
2026-07-16 19:32:33 +0300

    added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
+import { RouterProvider } from 'react-router-dom'
+import { routing } from '@/app/routing'
+
 export function App() {
   return (
-    <>
-    
-    </>
+    <RouterProvider router={routing} />
   )
 }
---
+++ src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,12 @@
+import { Wrapper } from '@/shared/components/Wrapper'
+import { Page } from '@/shared/components/Page'
+
+export function HomePage() {
+  return (
+    <Page>
+      <Wrapper>
+        Home
+      </Wrapper>
+    </Page>
+  )
+}
