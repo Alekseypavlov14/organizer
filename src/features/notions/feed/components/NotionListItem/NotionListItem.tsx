ef8d0dbbbdece901d4b1559417c0c7c3015001ee
YevheniiPavlov2006 <jackpavlov9@gmail.com>
2026-07-19 18:06:43 +0300

    added NotionItem components

---
+++ src/features/notions/feed/components/NotionListItem/NotionListItem.tsx
@@ -0,0 +1,27 @@
+import type { AbstractNotionItemProps } from '../../interfaces/AbstractNotionItemProps'
+import { Palette } from '@/shared/components/Palette'
+import styles from './NotionItem.module.css'
+
+interface NotionItemProps extends AbstractNotionItemProps {}
+
+export function NotionItem({
+  notion,
+  onClick = () => {}
+}: NotionItemProps) {
+  return (
+    <Palette 
+      className={styles.NotionItem} 
+      onClick={onClick}
+    >
+      <div className={styles.Header}>
+        <div className={styles.Title}>{notion.title}</div>
+        <div className={styles.Date}>22.11.2025</div>
+      </div>
+      <div className={styles.Body}>
+        <div className={styles.Description}>
+          {notion.description}
+        </div>
+      </div>
+    </Palette>
+  )
+}
