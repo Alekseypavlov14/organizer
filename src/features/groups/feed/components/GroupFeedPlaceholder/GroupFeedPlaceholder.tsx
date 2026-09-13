55318a6498e5f0c474ab29e66f950cb32a2b054c
Alekseypavlov14 <aleshapavlov9@gmail.com>
2026-09-13 22:58:49 +0300

    added default placeholder to group feed

--- src/features/groups/feed/components/GroupFeedPlaceholder/GroupFeedPlaceholder.tsx
+++ src/features/groups/feed/components/GroupFeedPlaceholder/GroupFeedPlaceholder.tsx
@@ -1,4 +1,5 @@
 import type { ReactNode } from 'react'
+import { groupFeedDefaultPlaceholder } from '../../constants'
 import { Placeholder } from '@/shared/components/Placeholder'
 import { Text } from '@/shared/components/Text'
 
@@ -6,7 +7,9 @@ interface GroupFeedPlaceholderProps {
   children?: ReactNode
 }
 
-export function GroupFeedPlaceholder({ children }: GroupFeedPlaceholderProps) {
+export function GroupFeedPlaceholder({ 
+  children = groupFeedDefaultPlaceholder
+}: GroupFeedPlaceholderProps) {
   return (
     <Placeholder>
       <Text>{children}</Text>
