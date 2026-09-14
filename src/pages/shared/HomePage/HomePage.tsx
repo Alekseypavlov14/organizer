e99b993b3bd6495a077e53aa59762fdab9082b15
Alekseypavlov14 <aleshapavlov9@gmail.com>
2026-09-14 18:18:57 +0300

    removed color modals

--- src/pages/shared/HomePage/HomePage.tsx
+++ src/pages/shared/HomePage/HomePage.tsx
@@ -19,8 +19,6 @@ import { isNull } from '@/shared/utils/validation'
 import { Icon } from '@/shared/components/Icon'
 import { Main } from '@/shared/components/Main'
 import { Text } from '@/shared/components/Text'
-import { ColorSelectionModal, useColorSelectionModal } from '@/features/colors/selection'
-import { ColorAddModal, useColorAddModal } from '@/features/colors/add'
 
 export function HomePage() {
   const { navigateNotionFeedPage, navigateNotionDisplayPage, navigateGroupFeedPage } = useNavigation()
@@ -30,9 +28,6 @@ export function HomePage() {
   const notionUrgentFeed = useNotionUrgentFeed()
   const notionImportantFeed = useNotionImportantFeed()
   const notionOtherFeed = useNotionOtherFeed()
-
-  const colorSelectionModal = useColorSelectionModal()
-  const colorAddModal = useColorAddModal()
 
   const notionsInSelectedDate = useNotionsStoreFilter(notion => (
     !isNull(notion.date) && isTheSameDate(notion.date?.value, calendar.store.selectedDate)
@@ -120,18 +115,7 @@ export function HomePage() {
         </Container>
       </Main>
 
-      <ColorSelectionModal />
-      <ColorAddModal />
-
       <FloatingActions>
-        <FloatingAction onClick={colorSelectionModal.open}>
-          <Icon name="album" size='l' />
-        </FloatingAction>
-
-        <FloatingAction onClick={colorAddModal.open}>
-          <Icon name="plus" size='l' />
-        </FloatingAction>
-
         <FloatingAction onClick={navigateGroupFeedPage}>
           <Icon name="folder" size='l' />
         </FloatingAction>
