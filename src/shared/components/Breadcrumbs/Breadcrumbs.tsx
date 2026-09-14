526c33e12b9a147fe971889f8cacb17fbcebbead
Alekseypavlov14 <aleshapavlov9@gmail.com>
2026-09-14 19:13:22 +0300

    added breadcrumbs styles

--- src/shared/components/Breadcrumbs/Breadcrumbs.tsx
+++ src/shared/components/Breadcrumbs/Breadcrumbs.tsx
@@ -1,11 +1,16 @@
+import type { ComponentProps } from 'react'
 import styles from './Breadcrumbs.module.css'
+import clsx from 'clsx'
 
-interface BreadcrumbsProps {}
+interface BreadcrumbsProps extends ComponentProps<'div'> {}
 
-export function Breadcrumbs({}: BreadcrumbsProps) {
+export function Breadcrumbs({ className, children, ...props }: BreadcrumbsProps) {
   return (
-    <div className={styles.Breadcrumbs}>
-
+    <div 
+      className={clsx(styles.Breadcrumbs, className)}
+      {...props}
+    >
+      {children}
     </div>
   )
 }
